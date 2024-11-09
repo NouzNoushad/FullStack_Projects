d9a999199c5be33636151a35ad7016114110d31e
unknown <nhd7noushadazeez@gmail.com>
2024-11-09 17:01:41 +0530

    update product

--- ecom_products_backend/controllers/products_controller.js
+++ ecom_products_backend/controllers/products_controller.js
@@ -1,5 +1,7 @@
 import productSchema from "../models/product_model.js"
 import fileSchema from "../models/file_model.js"
+import fs from "fs"
+import path from "path"
 
 // create new product
 export const createEcomProduct = async (req, res) => {
@@ -16,6 +18,9 @@ export const createEcomProduct = async (req, res) => {
                 })
             } else {
                 const { name, brand, price, description } = req.body
+                console.log(`name: ${name}, brand: ${brand}, price: ${price}, des: ${description}`)
+
+                // validations
                 if (!name) {
                     res.status(500).json({
                         message: "Name field is required",
@@ -32,6 +37,8 @@ export const createEcomProduct = async (req, res) => {
                         res.status(500).json({
                             message: "Product already exists"
                         })
+                        // delete file form local storage
+                        fs.unlinkSync(req.file.path)
                     } else {
                         // save image file
                         await fileSchema.create(req.file).then(async (image) => {
@@ -47,6 +54,11 @@ export const createEcomProduct = async (req, res) => {
                                 res.status(200).json({
                                     message: 'New product created'
                                 })
+                            }).catch((error) => {
+                                res.status(500).json({
+                                    message: "Failed to create product",
+                                    error
+                                })
                             })
                         })
                     }
@@ -56,7 +68,7 @@ export const createEcomProduct = async (req, res) => {
     } catch (error) {
         res.status(404).json({
             message: "Something went wrong",
-            error: error
+            error
         })
     }
 }
@@ -64,16 +76,105 @@ export const createEcomProduct = async (req, res) => {
 // get all products
 export const getEcomProducts = async (req, res) => {
     try {
-        await productSchema.find({}).then((products) => {
+        await productSchema.find().then((products) => {
             res.status(200).json({
                 message: `${products.length} items`,
                 products,
+            })
+        }).catch((error) => {
+            res.status(500).json({
+                message: "Failed"
             })
         })
     } catch (error) {
         res.status(404).json({
             message: 'Something went wrong',
             error: error,
+        })
+    }
+}
+
+// get product
+export const getEcomProductById = async (req, res) => {
+    try {
+        const { id } = req.params
+        await productSchema.findById(id).then((product) => {
+            if (product) {
+                res.status(200).json({
+                    message: "Product found",
+                    product,
+                })
+            }
+        }).catch((error) => {
+            res.status(500).json({
+                message: "Product not found"
+            })
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: 'Something went wrong',
+            error: error,
+        })
+    }
+}
+
+// update product
+export const updateEcomProduct = async (req, res) => {
+    try {
+        const { id } = req.params
+        const updatedProduct = { $set: {} }
+
+        req.upload(req, res, async (error) => {
+            const { name, brand, price, description, image } = req.body
+            if (error) {
+                res.status(404).json({
+                    message: `File upload error: ${error}`
+                })
+            } else if (req.file) {
+                const imageFile = await fileSchema.findOne(image)
+                const imagePath = path.join(imageFile.path)
+                console.log(`image path: ${imagePath}`)
+
+                // find existing image in local and delete
+                if (fs.existsSync(imagePath)) {
+                    fs.unlinkSync(imagePath)
+                }
+                // delete existing image in database
+                await fileSchema.findOneAndDelete(image)
+
+                // save file image
+                await fileSchema.create(req.file).then((image) => {
+                    updatedProduct.$set = { image: image._id }
+
+                    const newImagePath = path.join('public/images', req.file.filename)
+                    console.log(`new image path: ${newImagePath}`)
+                    fs.renameSync(req.file.path, newImagePath)
+                })
+            }
+
+            if (name) updatedProduct.$set.name = name
+            if (brand) updatedProduct.$set.brand = brand
+            if (price) updatedProduct.$set.price = price
+            if (description) updatedProduct.$set.description = description
+
+            // update product
+            await productSchema.findOneAndUpdate({ _id: id }, updatedProduct, { new: true }).then((product) => {
+                res.status(200).json({
+                    message: "Product updated",
+                    product,
+                })
+            }).catch((error) => {
+                res.status(500).json({
+                    message: "Failed to update product",
+                    error
+                })
+            })
+
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: 'Something went wrong',
+            error
         })
     }
 }
--- ecom_products_backend/routes/products_route.js
+++ ecom_products_backend/routes/products_route.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createEcomProduct, getEcomProducts } from "../controllers/products_controller.js"
+import { createEcomProduct, getEcomProductById, getEcomProducts, updateEcomProduct } from "../controllers/products_controller.js"
 import { uploadImageMiddleware } from "../middlewares/upload_middleware.js"
 
 const router = express.Router()
 
 router.post('/create_ecom_product', uploadImageMiddleware, createEcomProduct)
 router.get('/get_ecom_products', getEcomProducts)
+router.get('/get_ecom_product/:id', getEcomProductById)
+router.post('/update_ecom_product/:id', uploadImageMiddleware, updateEcomProduct)
 
 export default router
