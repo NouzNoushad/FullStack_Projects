2806fdcf42aca6db1a9e448b50b2b2fbe68165cf
unknown <nhd7noushadazeez@gmail.com>
2024-11-10 23:07:11 +0530

    crud

--- datas/app/(root)/add/page.tsx
+++ datas/app/(root)/add/page.tsx
@@ -1,29 +1,59 @@
 'use client'
 
 import { addProductAction } from '@/app/actions/addProduct';
-import { redirect } from 'next/navigation'
-import React, { useState } from 'react'
+import { getProductAction } from '@/app/actions/getProducts';
+import { updateProductAction } from '@/app/actions/updateProduct';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 export default function AddProduct() {
     const [name, setName] = useState<string>("");
-    const [brand, setBrand] = useState<string>("");
+    const [brand, setBrand] = useState<string | undefined>("");
     const [price, setPrice] = useState<string>("");
-    const [description, setDescription] = useState<string>("");
+    const [description, setDescription] = useState<string | undefined>("");
+    const router = useRouter();
+    const searchParam = useSearchParams()
+    const id = searchParam.get('id')
 
     const handleAddProduct = async () => {
         try {
-            await addProductAction({ name, brand, price: parseFloat(price) || 0, description }).then(() => {
-                setName("")
-                setBrand("")
-                setPrice("")
-                setDescription("")
-                redirect('/')
-            })
+            const productData = { name, brand, price: parseFloat(price) || 0, description };
+            if (id) {
+                await updateProductAction(id, productData);
+            } else {
+                await addProductAction(productData);
+            }
+
+            setName("")
+            setBrand("")
+            setPrice("")
+            setDescription("")
+            router.push('/')
+
         } catch (error) {
             console.log(`Error adding product: ${error}`)
         }
     }
 
+    const fetchProduct = async () => {
+        try {
+            if (id) {
+                await getProductAction(id).then((product) => {
+                    setName(product.name);
+                    setBrand(product.brand);
+                    setPrice(product.price.toString())
+                    setDescription(product.description)
+                })
+            }
+        } catch (error) {
+            console.log(`Error fetching product: ${error}`)
+        }
+    }
+
+    useEffect(() => {
+        console.log(id)
+        fetchProduct();
+    }, [id])
     return (
         <section className='py-[5rem]'>
             <div className="max-w-[600px] mx-auto px-5 xl:px-0">
@@ -33,7 +63,7 @@ export default function AddProduct() {
                     <input type="number" placeholder='Price' className='w-full px-5 py-3 rounded-md border border-green-300 outline-green-300' value={price} onChange={(e) => setPrice(e.target.value)} />
                     <textarea rows={4} placeholder='Description' className='w-full px-5 py-3 rounded-md border border-green-300 outline-green-300' value={description} onChange={(e) => setDescription(e.target.value)} />
                     <div className="flex md:justify-end justify-center">
-                        <button onClick={handleAddProduct} className='border border-green-300 text-green-300 uppercase font-bold text-[0.9rem] px-10 py-3 rounded-md transition-all duration-500 hover:bg-green-300 hover:text-white'>Add Product</button>
+                        <button onClick={handleAddProduct} className='border border-green-300 text-green-300 uppercase font-bold text-[0.9rem] px-10 py-3 rounded-md transition-all duration-500 hover:bg-green-300 hover:text-white'>{id ? "Update Product" : "Add Product"}</button>
                     </div>
                 </div>
             </div>
--- datas/app/api/products/route.ts
+++ datas/app/api/products/route.ts
@@ -1,4 +1,4 @@
-import Product from "@/models/Product";
+import Product from "@/app/models/Product";
 import connectMongodb from "../db"
 import { NextResponse } from "next/server";
 
