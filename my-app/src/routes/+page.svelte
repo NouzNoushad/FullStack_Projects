ab230b2a9df3b0d4e54e09a16fe680a4e2208696
unknown <nhd7noushadazeez@gmail.com>
2024-11-13 11:50:33 +0530

    create and get products

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,48 @@
-<script>
+<script lang="ts">
+    import { writable } from "svelte/store";
     import "../app.css";
+    import { onMount } from "svelte";
+
+    interface Product {
+        _id: string;
+        name: string;
+        brand: string;
+        price: number;
+        description: string;
+    }
+
+    const products = writable<Product[]>([]);
+
+    const fetchProducts = async (): Promise<Product[]> => {
+        const response = await fetch("/api/products");
+        const data = response.json();
+        return data;
+    };
+
+    const getProducts = async () => {
+        const data: Product[] = await fetchProducts();
+        products.set(data);
+    };
+
+    onMount(getProducts);
 </script>
 
-<h1 class="text-[5rem] text-red-500 font-bold h-screen flex items-center justify-center">Welcome to SvelteKit</h1>
-
+<main class="py-[5rem]">
+    <div class="max-w-[1200px] mx-auto px-5 xl:px-0">
+        <ul
+            class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[10px]"
+        >
+            {#each $products as product}
+                <li class="bg-black text-white px-5 py-5 rounded-md">
+                    <a href="/" class="space-y-1">
+                        <h1 class="text-[1.2rem] font-[500]">{product.name}</h1>
+                        <h3 class="font-[500] text-[0.9rem]">
+                            {product.brand}
+                        </h3>
+                        <p class="text-[1.3rem] font-bold">${product.price}</p>
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </div>
+</main>
--- my-app/src/routes/add/+page.svelte
+++ my-app/src/routes/add/+page.svelte
@@ -1,3 +1,69 @@
-<section>
-    <h1>Add Page</h1>
+<script lang="ts">
+    import { goto } from "$app/navigation";
+
+    let name = "";
+    let brand = "";
+    let price = 0;
+    let description = "";
+
+    const addProduct = async () => {
+        await fetch("/api/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                brand,
+                price,
+                description,
+            }),
+        });
+        goto("/");
+        name = "";
+        brand = "";
+        price = 0;
+        description = "";
+    };
+</script>
+
+<section class="py-[5rem]">
+    <div class="max-w-[600px] mx-auto px-5 xl:px-0">
+        <form
+            on:submit|preventDefault={addProduct}
+            class="border-2 border-black bg-slate-50 rounded-md px-8 py-10 space-y-2"
+        >
+            <input
+                type="text"
+                placeholder="Name"
+                class="px-2 py-2 w-full rounded-md border outline-none focus:border-black"
+                bind:value={name}
+            />
+            <input
+                type="text"
+                placeholder="Brand"
+                class="px-2 py-2 w-full rounded-md border outline-none focus:border-black"
+                bind:value={brand}
+            />
+            <input
+                type="number"
+                placeholder="Price"
+                class="px-2 py-2 w-full rounded-md border outline-none focus:border-black"
+                bind:value={price}
+            />
+            <textarea
+                rows={4}
+                placeholder="Name"
+                class="px-2 py-2 w-full rounded-md border outline-none focus:border-black"
+                bind:value={description}
+            ></textarea>
+            <div class="flex justify-center md:justify-end pt-5">
+                <button
+                    type="submit"
+                    class="border border-slate-200 bg-white rounded-md px-[50px] py-[10px] uppercase text-[0.9rem] font-[500] transition-all duration-300 hover:border-black"
+                    >Add Product</button
+                >
+            </div>
+        </form>
+    </div>
 </section>
