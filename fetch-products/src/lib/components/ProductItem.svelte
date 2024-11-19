06b7e4cf903164eb842c86c8506ab9c67812b779
unknown <nhd7noushadazeez@gmail.com>
2024-11-19 21:43:05 +0530

    cart add delete get

--- fetch-products/src/lib/components/ProductItem.svelte
+++ fetch-products/src/lib/components/ProductItem.svelte
@@ -1,8 +1,10 @@
 <script>
+	import { addToCart } from '$lib/store/cart/addCart';
+
 	const { product } = $props();
 </script>
 
-<div class="border border-black rounded-md shadow-lg px-5 py-5 flex flex-col">
+<div class="border border-black rounded-md shadow-lg px-5 py-5 flex flex-col cursor-pointer">
 	<div class="grow">
 		<img src={product.image} alt="" class="min-[400px]:h-[400px] mx-auto sm:h-[200px]" />
 		<h2 class="mt-4 text-[1.1rem] font-[500]">{product.title}</h2>
@@ -20,7 +22,10 @@
 			<span class="text-[0.8rem] text-gray-400">({product.rating.rate})</span>
 		</div>
 	</div>
-	<button class="mt-5 bg-green-500 w-full rounded-md px-2 py-2 text-white font-[500] space-x-3">
+	<button
+		onclick={() => addToCart(product)}
+		class="mt-5 bg-green-500 w-full rounded-md px-2 py-2 text-white font-[500] space-x-3"
+	>
 		<span>Add To Cart</span>
 		<i class="fa-solid fa-cart-shopping mr-2"></i>
 	</button>
