8499f96c0a018db9ac4011a977cd0c5a6984ec1a
unknown <nhd7noushadazeez@gmail.com>
2024-12-12 13:18:53 +0530

    right section

---
+++ my-blog/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='before:absolute before:left-[20px] before:w-[calc(100%-40px)] before:h-[0.08rem] before:bg-gray-300'>
+        <div className="py-[5rem]">
+
+        </div>
+    </footer>
+  )
+}
